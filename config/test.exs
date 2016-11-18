e905d8085ed4371c5a171247a71d4ca7481bec1a
Jorge Figueroa <yknx.4.b@gmail.com>
2016-11-18 12:03:42 -0600

    Setup comeonin

--- config/test.exs
+++ config/test.exs
@@ -17,3 +17,6 @@ config :alenx_blog_engine, AlenxBlogEngine.Repo,
   database: "alenx_blog_engine_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+config :comeonin, :bcrypt_log_rounds, 4
+config :comeonin, :pbkdf2_rounds, 1
