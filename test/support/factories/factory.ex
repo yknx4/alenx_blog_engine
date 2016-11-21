be96d7003d59b1b086e77583e8423312f19e7802
Jorge Figueroa <yknx.4.b@gmail.com>
2016-11-21 14:00:21 -0600

    WIP

--- test/support/factories/factory.ex
+++ test/support/factories/factory.ex
@@ -2,6 +2,15 @@ defmodule AlenxBlogEngine.Factory do
   alias AlenxBlogEngine.{Repo, User, Post}
   use ExMachina.Ecto, repo: Repo
 
+  def post_factory do
+    %Post{
+      user: build(:user),
+      title: sequence(:title, &"title-#{&1}"),
+      description: "description",
+      draft: false
+    }
+  end
+
   def user_factory do
     password = "pa4ssw0rd"
     %User{
