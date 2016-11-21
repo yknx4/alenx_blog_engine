bdcebc8fb8be2b79f947024eceb0ba69d1eaae8e
Jorge Figueroa <yknx.4.b@gmail.com>
2016-11-21 09:25:42 -0600

    Create session

---
+++ priv/repo/migrations/20161118222117_create_session.exs
@@ -0,0 +1,17 @@
+# priv/repo/migrations/20160120043602_create_session.exs
+defmodule AlenxBlogEngine.Repo.Migrations.CreateSession do
+  use Ecto.Migration
+
+  def change do
+    create table(:sessions) do
+      add :token, :string
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps
+    end
+
+    create index(:sessions, [:user_id])
+    create index(:sessions, [:token])
+
+  end
+end
